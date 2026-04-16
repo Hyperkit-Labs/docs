46ff96be91230fe1b90f087ab3779e20b8afa181
JustineDevs <TraderGOfficial@gmail.com>
2026-04-16 15:41:27 +0800

    update: modify page.tsx in specifications

--- app/erc1066-x402/specifications/page.tsx
+++ app/erc1066-x402/specifications/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-import { DocsHeader } from '@/components/pages/docs-header';
 import { DocsSidebar } from '@/components/pages/docs-sidebar';
 import { DocsTOC } from '@/components/pages/docs-toc';
 import { ArrowRight, ArrowLeft, FileText, Network, CreditCard, Layers } from 'lucide-react';
@@ -17,7 +16,6 @@ export default function SpecificationsPage() {
 
   return (
     <div className="min-h-screen flex flex-col bg-[#05050A] text-slate-400 font-sans antialiased">
-      <DocsHeader onMenuToggle={() => {}} />
       
       <div className="flex flex-1 pt-14 w-full max-w-[1600px] mx-auto">
         <DocsSidebar />
